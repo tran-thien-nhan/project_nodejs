3c18bd2857767b348090b272b2ce0e336b66b9ad
tran-thien-nhan <pipclupnomad@gmail.com>
2023-11-04 15:47:59 +0700

    xong middleware phần nào

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -49,11 +49,65 @@ const signup = async (req, res) => {
         })
 }
 
+const getDetailUser = (req, res) => {
+    //dựa vào session để in thông tin ra
+    res.render('user/detail', { title: 'thông tin người dùng', layout: 'layouts/userLayout', data: null, errors: null, user: req.session.user });
+}
+
+const getFormUpdateUser = async (req, res) => {
+    const { id } = req.params;
+    await User.findById(id)
+        .then(result => {
+            res.render('user/update', { title: 'Trang cập nhật thông tin người dùng', layout: 'layouts/userLayout', data: result, errors: null, user: req.session.user });
+        })
+        .catch(err => {
+            res.render('user/detail', { title: 'thông tin người dùng', layout: 'layouts/userLayout', data: null, errors: null, user: req.session.user });
+        })
+}
+
+const updateUser = async (req, res) => {
+    const { id, name, email, password, phone, address, gender, role } = req.body;
+
+    // Find the user by id and update their information
+    try {
+        const user = await User.findByIdAndUpdate(id, {
+            name,
+            email,
+            password,
+            phone,
+            address,
+            gender,
+            role
+        }, { new: true, runValidators: true });
+
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        // Update the user data in the session
+        req.session.user = user;
+
+        req.session.message = "User updated successfully";
+        res.redirect("/user/detail");
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            let errors = {};
+            for (const field in err.errors) {
+                errors[field] = err.errors[field].message;
+            }
+            res.render('user/update', { errors, data: req.body });
+        } else {
+            res.status(500).send("Internal Server Error");
+        }
+    }
+}
+
+
 const logout = (req, res) => {
     req.session.destroy();
     res.redirect('/user/login');
 }
 
 module.exports = {
-    viewUserIndex, signupForm, loginForm, signup, checkLogin, logout
+    viewUserIndex, signupForm, loginForm, signup, checkLogin, logout, getDetailUser, getFormUpdateUser, updateUser
 }
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { viewUserIndex, signupForm, loginForm, signup, checkLogin, logout } = require('../controllers/userController');
+const { viewUserIndex, signupForm, loginForm, signup, checkLogin, logout, getDetailUser, getFormUpdateUser, updateUser } = require('../controllers/userController');
 
 const upload = require('../controllers/middleware/uploadFile');
 const userRouter = express.Router();
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 // Middleware to set the user variable based on the session
 userRouter.use((req, res, next) => {
   if (req.session.user) {
-      res.locals.user = req.session.user; // Set 'user' to res.locals to make it available in templates
+    res.locals.user = req.session.user; // Set 'user' to res.locals to make it available in templates
   }
   next();
 });
@@ -18,5 +18,8 @@ userRouter.post('/signup', signup);
 userRouter.get('/login', loginForm);
 userRouter.post('/login', checkLogin);
 userRouter.get('/logout', logout);
+userRouter.get('/detail', getDetailUser);
+userRouter.get('/update/:id', getFormUpdateUser);
+userRouter.post('/update/:id', updateUser);
 
 module.exports = userRouter;
